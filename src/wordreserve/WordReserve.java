f6d06cf54419ad1c5819832c42814d83eb3881e9
patmukun <patmukun@sheridancollege.ca>
2024-09-23 13:02:45 -0400

    commit 1

---
+++ src/wordreserve/WordReserve.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package wordreserve;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mukun
+ */
+public class WordReserve {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         Scanner input = new Scanner(System.in);
+        
+        System.out.println("Please enter a word");
+        
+        String word = input.nextLine();
+        
+        char[] wordArray = new char[word.length()];
+        
+        for (int i = 0; i < word.length(); i++) {
+            
+            wordArray[i] = word.charAt(i);
+        }
+        
+        String wordReverse = "";
+        
+        for (int i = wordArray.length - 1; i >= 0; i--) {
+            
+            wordReverse += wordArray[i];
+            
+            System.out.print(wordArray[i]);
+        } 
+        
+        System.out.println("The reverse is: " + wordReverse);
+        // TODO code application logic here
+    }
+    
+}
